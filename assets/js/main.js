972cff7261cb956de726c3d8ebf345103dcbcb63
LeonardoFerrisi <biggestresurgence1492@gmail.com>
2023-03-19 19:00:10 -0400

    updated main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,8 +162,8 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 50,
-      backSpeed: 25,
+      typeSpeed: 100,
+      backSpeed: 50,
       backDelay: 1000
     });
   }
